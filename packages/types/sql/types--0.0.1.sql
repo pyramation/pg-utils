dbca54ef7dadbd44cd8ff6b65314c023348b5df7
Dan Lynch <pyramation@gmail.com>
2020-08-28 21:55:54 -0700

    simple upload type

--- packages/types/sql/types--0.0.1.sql
+++ packages/types/sql/types--0.0.1.sql
@@ -15,6 +15,10 @@ CREATE DOMAIN image AS jsonb CHECK ( (((value) ?& (ARRAY['url', 'mime'])) AND ((
 
 COMMENT ON DOMAIN image IS E'@name launchqlInternalTypeImage';
 
+CREATE DOMAIN upload AS text CHECK ( ((value) ~ ('^(https?)://[^\s/$.?#].[^\s]*$')) );
+
+COMMENT ON DOMAIN upload IS E'@name launchqlInternalTypeUpload';
+
 CREATE DOMAIN url AS text CHECK ( ((value) ~ ('^(https?)://[^\s/$.?#].[^\s]*$')) );
 
 COMMENT ON DOMAIN url IS E'@name launchqlInternalTypeUrl';
